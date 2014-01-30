471e8026ffecc5c39f4917e22ade330788bea1ae
ivan.soloviev@gmail.com <vano2103>
2014-01-30 17:07:02 +0400

    * Fixed tracking errors during uploading

--- routes/photos.js
+++ routes/photos.js
@@ -69,6 +69,9 @@ module.exports = {
                     }, 500);
                 });
 
+            } else {
+                console.log(err);
+                next();
             }
         });
 
